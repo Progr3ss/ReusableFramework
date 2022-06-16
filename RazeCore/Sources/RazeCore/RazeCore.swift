f5ddb1927422753c8517a4909d2baf3c1fb486dd
Martin Chibwe <mchibwe01@gmail.com>
2022-06-15 23:18:16 -0700

    first api

--- RazeCore/Sources/RazeCore/RazeCore.swift
+++ RazeCore/Sources/RazeCore/RazeCore.swift
@@ -1,6 +1,27 @@
-public struct RazeCore {
-    public private(set) var text = "Hello, World!"
+import UIKit
+import Foundation
 
-    public init() {
+class RazeCore {
+    
+    class func colorFromhexString(_ hexString: String) -> UIColor {
+        var cString:String = hexString.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+
+          if (cString.hasPrefix("#")) {
+              cString.remove(at: cString.startIndex)
+          }
+
+          if ((cString.count) != 6) {
+              return UIColor.gray
+          }
+
+          var rgbValue:UInt64 = 0
+          Scanner(string: cString).scanHexInt64(&rgbValue)
+
+          return UIColor(
+              red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+              green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+              blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+              alpha: CGFloat(1.0)
+          )
     }
 }
--- RazeCore/Tests/RazeCoreTests/RazeCoreTests.swift
+++ RazeCore/Tests/RazeCoreTests/RazeCoreTests.swift
@@ -2,10 +2,8 @@ import XCTest
 @testable import RazeCore
 
 final class RazeCoreTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(RazeCore().text, "Hello, World!")
+    func testColoreRedEaul() {
+        let color = RazeCore.colorFromhexString("FF0000")
+        XCTAssertEqual(color, .red)
     }
 }
