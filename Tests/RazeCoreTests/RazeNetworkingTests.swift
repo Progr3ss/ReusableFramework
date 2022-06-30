044d7da5dd7934d8de5172e0e031c2d5eb91bd8d
Martin Chibwe <mchibwe01@gmail.com>
2022-06-29 23:22:33 -0700

    Added first networking API for retrieving network data

--- Tests/RazeCoreTests/RazeNetworkingTests.swift
+++ Tests/RazeCoreTests/RazeNetworkingTests.swift
@@ -6,8 +6,27 @@
 //
 
 import XCTest
+import RazeCore
 
 class RazeNetworkingTests: XCTestCase {
+    
+    func testLoadDataCall() {
+        let manager = RazeCore.Networking.Manager()
+        let expectation = XCTestExpectation(description: "called for data")
+        guard let url  = URL(string: "https://www.raywenderlich.com/") else {
+            return XCTFail("Could not create URL properly")
+        }
+        
+        manager.loadData(from:url){ result in expectation.fulfill()
+            switch result {
+            case .success(let returnedData):
+                XCTAssertNotNil(returnedData, "Response data is nil")
+            case .failure(let error):
+                XCTFail(error?.localizedDescription ?? "error forming error")
+            }
+        }
+        wait(for: [expectation], timeout:5)
+    }
     
     func testExample() throws {
         // This is an example of a functional test case.
